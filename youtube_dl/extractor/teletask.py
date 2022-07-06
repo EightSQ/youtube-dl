0600718e7127ea46e5eddfa8a80e36c0444328a7
Otto Kissig <1622854+EightSQ@users.noreply.github.com>
2022-07-06 16:02:34 +0200

    [TeleTask] Implement new teletask downloader

--- youtube_dl/extractor/teletask.py
+++ youtube_dl/extractor/teletask.py
@@ -1,34 +1,50 @@
 from __future__ import unicode_literals
 
 import re
+import html
+import json
 
 from .common import InfoExtractor
 from ..utils import unified_strdate
 
 
 class TeleTaskIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?tele-task\.de/archive/video/html5/(?P<id>[0-9]+)'
+    _VALID_URL = r'https?://(?:www\.)?tele-task\.de/lecture/video/(?P<id>[0-9]+)'
     _TEST = {
-        'url': 'http://www.tele-task.de/archive/video/html5/26168/',
+        'url': 'http://www.tele-task.de/lecture/video/9313/',
         'info_dict': {
-            'id': '26168',
-            'title': 'Duplicate Detection',
+            'id': '9313',
+            'title': 'Dateisysteme',
         },
         'playlist': [{
-            'md5': '290ef69fb2792e481169c3958dbfbd57',
             'info_dict': {
-                'id': '26168-speaker',
+                'id': '9313-video',
                 'ext': 'mp4',
-                'title': 'Duplicate Detection',
-                'upload_date': '20141218',
+                'title': 'Dateisysteme',
+                'upload_date': '20220531',
+                'formats': [
+                    {
+                        'format_id': 'sd',
+                    },
+                    {
+                        'format_id': 'hd',
+                    }
+                ]
             }
         }, {
-            'md5': 'e1e7218c5f0e4790015a437fcf6c71b4',
             'info_dict': {
-                'id': '26168-slides',
+                'id': '9313-desktop',
                 'ext': 'mp4',
-                'title': 'Duplicate Detection',
-                'upload_date': '20141218',
+                'title': 'Dateisysteme',
+                'upload_date': '20220531',
+                'formats': [
+                    {
+                        'format_id': 'sd',
+                    },
+                    {
+                        'format_id': 'hd',
+                    }
+                ]
             }
         }]
     }
@@ -38,16 +54,29 @@ class TeleTaskIE(InfoExtractor):
         webpage = self._download_webpage(url, lecture_id)
 
         title = self._html_search_regex(
-            r'itemprop="name">([^<]+)</a>', webpage, 'title')
+            r'<title>([^<]+)</title>', webpage, 'title')
         upload_date = unified_strdate(self._html_search_regex(
-            r'Date:</td><td>([^<]+)</td>', webpage, 'date', fatal=False))
+            r'Date: ([^<]+) <br>', webpage, 'date', fatal=False))
 
+        player_info = json.loads(html.unescape(self._html_search_regex(
+            r'<video-player id=\"player\" configuration=\'([^\']+)\'>', webpage, 'player_info')))
+
+        entry_dict = {}
+        for stream_group in player_info["streams"]:
+            for stream_type, video_url in stream_group.items():
+                content_type, url_type = re.findall(r'\/([^\/]+)\.(mp4|m3u8)$', video_url)[0]
+                if content_type not in entry_dict.keys():
+                    entry_dict[content_type] = []
+                if url_type == 'mp4':
+                    entry_dict[content_type].append({
+                        'url': video_url,
+                        'format_id': stream_type
+                    })
         entries = [{
-            'id': '%s-%s' % (lecture_id, format_id),
-            'url': video_url,
+            'id': f'{lecture_id}-{content_type}',
             'title': title,
             'upload_date': upload_date,
-        } for format_id, video_url in re.findall(
-            r'<video class="([^"]+)"[^>]*>\s*<source src="([^"]+)"', webpage)]
+            'formats': formats
+        } for content_type, formats in entry_dict.items()]
 
         return self.playlist_result(entries, lecture_id, title)
